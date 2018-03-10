57a52835602f7e7081b93171a6dd1dfce70da10a
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2018-03-10 23:23:13 +0100

    Introduce fish config

---
+++ fish/functions/odc.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function odc --description 'alias odc=./projects/mbc/docker/odc.sh'
+	./projects/mbc/docker/odc.sh  $argv;
+end
---
+++ fish/functions/soe.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function soe --description 'alias soe=./projects/mbc/docker/soe.sh'
+	./projects/mbc/docker/soe.sh  $argv;
+end
